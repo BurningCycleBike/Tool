3ba2fc9082ddad7f21e2ac393a7199b47dbc0710
BurningCycleBike <104562844+BurningCycleBike@users.noreply.github.com>
2022-05-22 02:17:39 +0700

    autoRename fix

--- autoRename.py
+++ autoRename.py
@@ -7,6 +7,7 @@ GETPROCADDRESS = 0x1004102C
 
 def rename(funcAddr):
 	pseudoCode = str(ida_hexrays.decompile(funcAddr)).split('\n')
+	if len([i for i in pseudoCode if 'strcpy' in i]) > 1: return
 	newName = next(filter(lambda i: 'strcpy' in i, pseudoCode), None).split('\"')[1]
 	ida_name.set_name(funcAddr, 'inv_' + newName)
 
@@ -14,4 +15,5 @@ def autoRename():
 	xrefObj = idautils.XrefsTo(GETPROCADDRESS)
 	for a in xrefObj:
 		funcAddr = idaapi.get_func(a.frm).start_ea
+		if funcAddr == None: continue
 		rename(funcAddr)
